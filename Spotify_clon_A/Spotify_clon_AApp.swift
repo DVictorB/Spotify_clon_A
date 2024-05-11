ac64b3473d8e12a3dc55fa7c0980100458b26404
BellidoJeremy <jeremy.bellido@tecsup.edu.pe>
2024-05-11 13:38:56 -0500

    Initial Commit

---
+++ Spotify_clon_A/Spotify_clon_AApp.swift
@@ -0,0 +1,20 @@
+//
+//  Spotify_clon_AApp.swift
+//  Spotify_clon_A
+//
+//  Created by Mac14 on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_clon_AApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
